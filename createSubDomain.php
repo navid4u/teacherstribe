60366fdda1c2a4ce7dee74eec5234e100a7a18ec
navid.nasrollahi@yahoo.com <Nasr6620514>
2024-02-21 13:29:57 +0330

    fix create subdomain

--- createSubDomain.php
+++ createSubDomain.php
@@ -18,65 +18,17 @@ if ($stmt->num_rows > 0) {
     // Subdomain exists, show appropriate message
     echo "This domain is already taken. Please try another name!";
 } else {
-    // Subdomain does not exist, continue with the rest of your code
-    // ...
-    // ...
+
+
+$newDomain = "http://" . $subDomain . "." . $rootDomain . "";
+echo '<span style="font-weight: bold; color: orange; animation: flash 1s infinite;">Your domain is registered as: ' . $newDomain . '</span>';
 
 
 
-//  $buildRequest = "/frontend/x3/subdomain/doadddomain.html?rootdomain=" . $rootDomain . "&domain=" . $subDomain;
-
-
-//$buildRequest = "/frontend/paper_lantern/subdomain/doadddomain.html?rootdomain=" . $rootDomain . "&domain=" . $subDomain . "&dir=public_html/code/" . $subDomain;
-$buildRequest = "/frontend/paper_lantern/subdomain/doadddomain.html?rootdomain=" . $rootDomain . "&domain=" . $subDomain . "&dir =public_html/subdomains/";
-
-$openSocket = fsockopen('localhost', 2082);
-if (!$openSocket) {
-    return "Socket error";
-    exit();
-}
-
-$authString = $cPanelUser . ":" . $cPanelPass;
-$authPass = base64_encode($authString);
-$buildHeaders = "GET " . $buildRequest . "\r\n";
-$buildHeaders .= "HTTP/1.0\r\n";
-$buildHeaders .= "Host:localhost\r\n";
-$buildHeaders .= "Authorization: Basic " . $authPass . "\r\n";
-$buildHeaders .= "\r\n";
-
-fputs($openSocket, $buildHeaders);
-while (!feof($openSocket)) {
-    fgets($openSocket, 128);
-}
-fclose($openSocket);
-
-$newDomain = "http://" . $subDomain . "." . $rootDomain . "/";
-echo 'Your domain is registered as: ' . $newDomain;
 
 $query = "UPDATE ttep_teacher SET website = '{$newDomain}' WHERE username = '{$username}'";
 $result = mysqli_query($conn, $query);
 }
-
-
-
-
- $domain = "teacherstribe.net"; // Replace "example.com" with your own domain
-$folderPath = $subDomain;
-$content = "<?php
-// Redirect to the desired URL
-header('Location: http://www.teacherstribe.net/web_app/my_site/1/cv.php?username=$username');
-exit(); // Make sure to exit immediately after redirection
-?>
-
-";
-$filename = "index.php";
-
-// Create the PHP file with specific content.
-$file = fopen("/home/efqlckyo/". $folderPath . "/" . $filename, "w");
-
-fwrite($file, $content);
-fclose($file);
- 
 
 
 
